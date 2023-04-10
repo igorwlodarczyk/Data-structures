933086f4ff1867700366fe7c0928a71cc2c0fde4
Igor Wlodarczyk <wlodar.igor@gmail.com>
2023-04-10 15:01:49 +0200

    double linked list merged

--- main.cpp
+++ main.cpp
@@ -6,11 +6,6 @@ using std::cout;
 using std::endl;
 
 int main(){
-    DoublyLinkedList lista1;
-    lista1.addFirst(10);
-    lista1.addFirst(5);
-    for(int i = 0; i < lista1.getSize(); i++){
-        cout<<lista1.get(i)<<endl;
-    }
+    
     return 0;
 }
