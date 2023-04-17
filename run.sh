fbe29038d2b39308ae56de4bf7c1428d7f73b310
Igor Wlodarczyk <wlodar.igor@gmail.com>
2023-04-17 11:13:57 +0200

    updated run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # compile main program
-g++ -o main double_linked_list.cpp dynamic_array.cpp binary_heap.cpp binary_search_tree.cpp main.cpp
+g++ -std=c++11 -o main double_linked_list.cpp dynamic_array.cpp binary_heap.cpp binary_search_tree.cpp main.cpp
 
 # compile tests
 g++ -std=c++14 dynamic_array.cpp double_linked_list.cpp binary_heap.cpp binary_search_tree.cpp tests.cpp -lgtest -lgtest_main -pthread -o tests
